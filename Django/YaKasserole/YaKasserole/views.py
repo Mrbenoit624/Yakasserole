aa3f8d2576a842158139361e05abadac58a05ac2
Benoit Forgette <forget_c@epita.fr>
2017-03-13 11:07:08 +0100

    [DEVELOP][FRONT] Clean repo for front website

--- Django/YaKasserole/YaKasserole/views.py
+++ Django/YaKasserole/YaKasserole/views.py
@@ -4,5 +4,5 @@ from django.template import loader
 
 
 def index(request):
-    template = loader.get_template('html/index.html')
+    template = loader.get_template('index.html')
     return HttpResponse(template.render({}, request))
--- Django/YaKasserole/entreprise/views.py
+++ Django/YaKasserole/entreprise/views.py
@@ -1,8 +1,12 @@
 
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
 
 
 def page(request, page):
-    template = loader.get_template('html/entreprise/{}.html'.format(page))
-    return HttpResponse(template.render({}, request))
+    try:
+        template = loader.get_template('entreprise/{}.html'.format(page))
+        return HttpResponse(template.render({}, request))
+    except:
+        raise Http404('Page not found {}'.format(page))

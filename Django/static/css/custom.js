3b9ed7fc63c73a7950b3f027c950342530d706d1
Augustin Goldblum <augustin.goldblum@epita.fr>
2017-05-04 18:05:05 +0200

    [DEVELOP][DESIGN] Change location and base design

--- Django/static/css/custom.js
+++ Django/static/css/custom.js
@@ -1,4 +1,5 @@
 $.backstretch(["/../static/img/bg-03.jpg",
                "/../static/img/bg-01.jpg",
-               "/../static/img/navbar.jpeg"
-              ], {duration: 3000, fade: 750});
+               "/../static/img/navbar.jpeg",
+               "/../static/img/bg-02.jpg"
+              ], {duration: 6000, fade: 750});

20123c8a45828ded7456e062f7693dd1b2195381
Augustin Goldblum <augustin.goldblum@epita.fr>
2017-05-04 17:25:01 +0200

    [DEVELOP][CSS] Test changing image and navbar

--- Django/static/css/custom.js
+++ Django/static/css/custom.js
@@ -1,1 +1,4 @@
-$.backstretch("/../static/img/bg-03.jpg");
+$.backstretch(["/../static/img/bg-03.jpg",
+               "/../static/img/bg-01.jpg",
+               "/../static/img/navbar.jpeg"
+              ], {duration: 3000, fade: 750});
